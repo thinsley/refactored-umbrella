df32aa5b87816d89d0744c27f88c3382c39e44cb
Tristan Hinsley <thinsley@protonmail.com>
2022-06-13 17:52:00 -0400

    Small edit to hello-world, testing IDE commit msg

--- crash-course/hello-world.py
+++ crash-course/hello-world.py
@@ -2,6 +2,6 @@ print("Hello World, GOATIFI rules")
 
 firstName = "Tristan"
 lastName = "Hinsley"
-occupation = "Crab Raver"
+occupation = "Crab Raver "
 
 print("My name is " + firstName + " " + lastName + ", and I work as a " + occupation.upper())
